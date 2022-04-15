e084aefb36f5550dde821f1a74d6a4f565d906aa
lucas.felizardo <lucas.felizardo@vegamonitoramento.com.br>
2022-04-15 17:38:31 -0300

    feature: cria controel de login e logout

--- components/Login.js
+++ components/Login.js
@@ -1,17 +1,38 @@
 import { Box, Button, Center, Image, Stack } from "@chakra-ui/react";
-import Head from "next/head";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "../firebaseconfig";
 
 export default function Login() {
-    return (
-        <Center h='100vh'>
-            <Stack align='center' bgColor='purple.300' w='fit-content' p={5} rounded='3xl' spacing={5}>
-                <Box bgColor='white' w='fit-content' p={2} borderRadius='full'>
-                    <Image src='https://www.palladinobmw.com/wp-content/uploads/2019/07/google-minimal-logo.png' boxSize='50px'/>
-                </Box>
-                <Button bgColor='white' rounded='xl' boxShadow='md'>
-                    Entrar com o Google
-                </Button>
-            </Stack>
-        </Center>
-    )
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  return (
+    <Center h="100vh">
+      <Stack
+        align="center"
+        bgColor="purple.300"
+        w="fit-content"
+        p={5}
+        borderRadius="3xl"
+        spacing={5}
+        boxShadow="md">
+
+        <Box
+          bgColor="white"
+          w="fit-content"
+          p={2}
+          borderRadius="full">
+          <Image src="https://www.palladinobmw.com/wp-content/uploads/2019/07/google-minimal-logo.png" boxSize="50px"/>
+        </Box>
+
+        <Button
+          bgColor="white"
+          rounded="xl"
+          boxShadow="md"
+          onClick={() => signInWithGoogle("", {prompt: "select_account"})}>
+          Entrar com o Google
+        </Button>
+
+      </Stack>
+    </Center>
+  )
 }
