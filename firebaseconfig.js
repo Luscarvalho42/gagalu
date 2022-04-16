2e1a329e83a0a6e242766194c9c33da6e7abb683
lucas.felizardo <lucas.felizardo@vegamonitoramento.com.br>
2022-04-16 13:54:43 -0300

    bug: corrige problema de login em produção

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -4,9 +4,9 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDOspxA6hbe5_ZCpbfaHO4FCeJ7OBf-gk",
-  authDomain: "gagalu-48ceb.firebaseapp.com",
-  projectId: "gagalu-48ceb",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: "gagalu-48ceb.appspot.com",
   messagingSenderId: "1013329605864",
   appId: "1:1013329605864:web:c0744a4972a10c841bc55d"
